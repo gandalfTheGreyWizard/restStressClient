daa636441c829079bbf5245c77a35cace651f9b5
Soumik Chakraborty <soumik@datasutram.com>
2023-02-27 18:14:13 +0530

    Modules - Stress :- decoupled the configuration decode from utils module - Utils :- added a utility module to encapsulate request access and   polling added p_tqdm module for threading

--- stress.py
+++ stress.py
@@ -2,12 +2,16 @@ import requests
 import argparse
 import logging
 import json
+import math
 from collections import defaultdict
+from p_tqdm import p_map
 
 logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s', datefmt='%Y-%m-%d:%H:%M:%S', level=logging.INFO)
 
 parser = argparse.ArgumentParser('Processing')
 parser.add_argument('-c', '--config', help='json configuration path to be used by stress client', type=str)
+parser.add_argument('-n', '--num_cpu', help='number of processing threads to be used', type=int)
+parser.add_argument('-s', '--sample_len', help='length of the query sample to be stress tested with', type=int)
 logger = logging.getLogger('Stress')
 
 class Stress:
@@ -18,40 +22,46 @@ class Stress:
         except Exception as e:
             logger.error(e)
             logger.exception(e)
-    def create_headers(self, token):
-        headers = {
-                'Authorization': 'Bearer {}'.format(token),
-                'Content-Type': 'application/json'
-                }
-        return headers
-    def get_request(self, url, req_headers):
-        request_obj = requests.get(url, headers=req_headers)
-        response_dict = request_obj.json()
-        logger.info(response_dict)
-    def post_request(self, url, body, headers):
-        request_obj = requests.post(url, body, headers)
-        response_dict = request_obj.json()
-        logger.info(response_dict)
     def show_config(self):
         logger.info(json.dumps(self.config_dict))
-    def process_requests(self):
-        for each_request_object in self.config_dict['requests']:
-            logger.info(each_request_object)
-            request_headers = self.config_dict['config']['headers'][each_request_object['headers']]
-            if each_request_object['type'] == 'get':
-                self.get_request(each_request_object['url'], json.dumps(request_headers)),
-            elif each_request_object['type'] == 'post':
-                self.post_request(each_request_object['url'], each_request_object['body'], request_headers)
-            else:
-                logger.erro('problem parsing request object {}'.format(each_request_object))
+    def export_requests(self, num_reqs):
+        self.requests_to_process = []
+        current_length = len(self.config_dict['requests'])
+        iterations = math.floor(num_reqs / current_length)
+        temp_dict_arr = []
+        for i in range(0, current_length):
+            self.config_dict['requests'][i]['headers'] = self.config_dict['config']['headers'][self.config_dict['requests'][i]['headers']]
+            temp_dict_arr += [self.config_dict['requests'][i]] * iterations
+        utility_object = Utils()
+        response_objects = p_map(utility_object.process_requests, temp_dict_arr, num_cpus=64)
+        print(response_objects)
+
+class Utils:
+    def get_request(self, url, req_headers):
+        request_obj = requests.get(url, headers=req_headers)
+        return request_obj
+        # logger.info(response_dict)
+    def post_request(self, url, body, headers):
+        request_obj = requests.post(url, body, headers)
+        return request_obj
+        # logger.info(response_dict)
+    def process_requests(self, each_request_object):
+        if each_request_object['type'] == 'get':
+            return self.get_request(each_request_object['url'], each_request_object['headers'])
+        elif each_request_object['type'] == 'post':
+            return self.post_request(each_request_object['url'], each_request_object['body'], each_request_object['headers'])
+        else:
+            logger.erro('problem parsing request object {}'.format(each_request_object))
+
 if __name__ == '__main__':
     args = parser.parse_args()
+    utils_object = Utils()
     if args.config:
         stress_client = Stress(args.config)
     else:
         stress_client = Stress()
     # stress_client.show_config()
-    stress_client.process_requests()
+    stress_client.export_requests(args.sample_len)
     # if args.config:
         # logging.info(args.config)
     # else:
